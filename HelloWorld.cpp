1b4cd566b8120f302b0a824f8165556d7a7ea359
Lalith Souri Vummadi <lvu719@uregina.ca>
2022-09-29 00:46:30 -0600

    Last Commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Name: Lalith Souri Vummadi
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello" << endl;
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# I am
+## *Trippin Trippin....*
+### :beers:
